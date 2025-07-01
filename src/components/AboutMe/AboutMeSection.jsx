703271f2cd5ced29d765703d15d82080563de0da
araldev <arturo.r2d2.dev@gmail.com>
2025-07-01 19:43:55 +0200

    updates

--- src/components/AboutMe/AboutMeSection.jsx
+++ src/components/AboutMe/AboutMeSection.jsx
@@ -1,4 +1,6 @@
 import styles from './AboutMeSection.module.css'
+import avatar from '../../assets/yo-sin-fondo-M.webp'
+import brand from '../../assets/brand-araldev.webp'
 
 export function AboutMeSection () {
   return (
@@ -6,8 +8,14 @@ export function AboutMeSection () {
       <h2>About Me</h2>
 
       <aside className={styles.grid_container}>
-        <img src='' alt='' />
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi ad sapiente error, quia enim id. A, voluptas recusandae! Hic magni porro accusamus! Quasi enim fugiat sed accusamus, dicta magni.</p>
+        <img className={styles.avatar_image} src={avatar} alt={'Arturo\'s photo'} />
+        <img className={styles.brand_image} src={brand} alt='Araldev Brand' />
+        <div className={styles.text_container}>
+          <p>Mi nombre es Arturo, aunque muchos me conocen como R2. Estudié TSAFAD, pero tras varios años dedicándome a ello, decidí dar un giro hacia el sector tecnológico —otra de mis grandes pasiones— impulsado por mi interés en crear, construir y resolver.</p>
+          <p>Desde pequeño he estado en contacto con ordenadores, aunque no fue hasta 2022 cuando comencé a adentrarme en el mundo de la programación. En enero de 2025, tomé la decisión de formarme de manera seria y estructurada. Desde entonces, he desarrollado múltiples proyectos y tengo otros tantos en mente que me gustaría llevar a cabo y monetizar.</p>
+          <p>Me considero una persona autodidacta; aprender por mi cuenta ha sido una constante en mi vida y eso me ha convertido en alguien resolutivo, independiente y siempre en busca de nuevas soluciones.</p>
+          <p>Además, soy una persona muy activa y deportista. Mantenerme en forma no solo me ayuda a estar bien físicamente, sino que también potencia mi enfoque, energía y claridad mental para afrontar cualquier desafío.</p>
+        </div>
 
       </aside>
     </section>
