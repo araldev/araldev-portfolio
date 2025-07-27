0b5b5771be76c5c817819ed115d90a8d593811bb
araldev <arturo.r2d2.dev@gmail.com>
2025-07-27 19:59:31 +0200

    add npm icon

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -165,6 +165,12 @@ export const STORYBOOK_ICON = (
   </svg>
 )
 
+export const NPM_ICON = (
+  <svg data-key={key.npm} viewBox='0 0 780 250' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='white'>
+    <path d='M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z' strokeWidth='5' stroke='white' />
+  </svg>
+)
+
 // Utils Svg
 export const ARROW_UP = (
   <svg data-key={key.arrowUp} xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='1' strokeLinecap='round' strokeLinejoin='round'>
